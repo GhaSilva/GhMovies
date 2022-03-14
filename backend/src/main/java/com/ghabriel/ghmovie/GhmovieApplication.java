4f065eb7aa58f839559786477a4ef68af152ed8d
GhaSilva <ghabriel-silva@outlook.com>
2022-03-14 11:52:37 -0300

    first commit

---
+++ backend/src/main/java/com/ghabriel/ghmovie/GhmovieApplication.java
@@ -0,0 +1,13 @@
+package com.ghabriel.ghmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ghabriel/ghmovie/GhmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghabriel.ghmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
